a42c80b27c60261ddb742f7e50e331db8a58a267
MacStudent <macstudent@20X-iMacXX-M-419.local>
2018-07-23 17:26:26 -0400

    final'

---
+++ gagan/gagan/gagan/Classes/Product.swift
@@ -0,0 +1,10 @@
+//
+//  Product.swift
+//  gagan
+//
+//  Created by MacStudent on 2018-07-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+//Sir has Checked the Project
---
+++ gagan/gagan/gagan/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  gagan
+//
+//  Created by MacStudent on 2018-07-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+////Sir has Checked the Project
